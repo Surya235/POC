17e516e17f930aed9f50f1273fb7683bc35524d8
Surya Manikandan <73686015+Surya235@users.noreply.github.com>
2020-11-26 11:58:45 +0530

    Update search.php

--- search.php
+++ search.php
@@ -17,41 +17,39 @@
                 <h1 class="page-header">
                     Welcome to the Blog
                 </h1>
-				<?php
-						if(isset($_POST['submit_search'])){
-							$search = $_POST['search'];
-							
-							$query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-							$search_post = mysqli_query($conn, $query);
-							
-							$count = mysqli_num_rows($search_post);
-							if($count == 0 ){ 
-							echo "<br> <h2>NO RESULT</h2>";
-							}
-							else{
-								while($row = mysqli_fetch_assoc($search_post)){
-								$post_id = $row["post_id"];
-								$post_title = $row["post_title"];
-								$post_author = $row["post_author"];
-								$post_date = $row["post_date"];
-								$post_image = $row["post_image"];
-								$post_content = $row["post_content"];
-							?>
-							<!--Blog Post -->
-							<h2>
-								<a href="post.php?post_det_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
-							</h2>
-							<p class="lead">
-								by <a href="author.php?author=<?php echo $post_author;?>"><?php echo $post_author ?></a>
-							</p>
-							<p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-							<hr>
-							<img class="img-responsive" src="images/<?php echo $post_image;?>" alt="image">
-							<hr>
-							<p><?php echo $post_content ?></p>
-						<?php } } }?>
-					
-				
+		<?php
+		if(isset($_POST['submit_search'])){
+			$search = $_POST['search'];
+
+			$query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+			$search_post = mysqli_query($conn, $query);
+
+			$count = mysqli_num_rows($search_post);
+			if($count == 0 ){ 
+			echo "<br> <h2>NO RESULT</h2>";
+			}
+			else{
+				while($row = mysqli_fetch_assoc($search_post)){
+				$post_id = $row["post_id"];
+				$post_title = $row["post_title"];
+				$post_author = $row["post_author"];
+				$post_date = $row["post_date"];
+				$post_image = $row["post_image"];
+				$post_content = $row["post_content"];
+			?>
+			<!--Blog Post -->
+			<h2>
+				<a href="post.php?post_det_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
+			</h2>
+			<p class="lead">
+				by <a href="author.php?author=<?php echo $post_author;?>"><?php echo $post_author ?></a>
+			</p>
+			<p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+			<hr>
+			<img class="img-responsive" src="images/<?php echo $post_image;?>" alt="image">
+			<hr>
+			<p><?php echo $post_content ?></p>
+		<?php } } }?>		
 					
 			</div>
 
